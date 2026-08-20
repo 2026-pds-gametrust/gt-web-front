3be6a531f0fa979826d56c3e9d6803833e8bf694
FilipePaixao <fpedrapaixao@gmail.com>
2026-08-19 21:16:06 -0300

    feat(shared): add 06-shared/lib/format/format-money.ts

---
+++ src/06-shared/lib/format/format-money.ts
@@ -0,0 +1,6 @@
+export function formatMoney(cents: number, currency = 'BRL'): string {
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency,
+  }).format(cents / 100);
+}
