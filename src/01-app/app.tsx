e87b2856114ed72307a91c2bdfcb87252453c08c
FilipePaixao <fpedrapaixao@gmail.com>
2026-08-19 23:44:34 -0300

    feat(app): integrate error boundary and updated providers

--- src/01-app/app.tsx
+++ src/01-app/app.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter } from 'react-router-dom';
 import { AppRouter } from '@app/providers/app-router';
+import { ErrorBoundary } from '@app/providers/error-boundary';
 import { SessionProvider } from '@app/providers/session-provider';
 
 export function App() {
   return (
     <BrowserRouter>
-      <SessionProvider>
-        <AppRouter />
-      </SessionProvider>
+      <ErrorBoundary>
+        <SessionProvider>
+          <AppRouter />
+        </SessionProvider>
+      </ErrorBoundary>
     </BrowserRouter>
   );
 }
