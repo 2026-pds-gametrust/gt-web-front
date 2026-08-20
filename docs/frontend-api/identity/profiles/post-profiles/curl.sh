1663546edd3a46e1e4f5b57526f49d55c50a8e01
FilipePaixao <fpedrapaixao@gmail.com>
2026-08-19 21:15:10 -0300

    docs(frontend-api): add identity/profiles/post-profiles/curl.sh

---
+++ docs/frontend-api/identity/profiles/post-profiles/curl.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# Create a profile
+curl -X POST 'http://localhost:3000/profiles' \
+  -H 'Accept: application/json' \
+  -H 'Authorization: Bearer <access_token>' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "id": "string",
+  "userId": "string",
+  "displayName": "string",
+  "bio": "string",
+  "locationApprox": "string",
+  "addresses": [
+    {
+      "id": "string",
+      "label": "string",
+      "recipientName": "string",
+      "postalCode": "string",
+      "street": "string",
+      "number": "string",
+      "complement": "string",
+      "district": "string",
+      "city": "string",
+      "state": "string",
+      "country": "BR",
+      "isBilling": false,
+      "isShipping": false
+    }
+  ],
+  "defaultShippingAddressId": "string",
+  "setupItems": [
+    {}
+  ]
+}'
